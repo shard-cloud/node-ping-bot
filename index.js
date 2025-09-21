99eea269a55dba08b11ca873f32145fe66348379
Shard Cloud <alert@shardcloud.app>
2025-09-21 20:26:57 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const prefix = process.env.PREFIX || "!";
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === `${prefix}ping`) {
+    message.reply(`🏓 Pong! WS Ping: ${client.ws.ping}ms`);
+  }
+});
+
+client.login(process.env.TOKEN);
